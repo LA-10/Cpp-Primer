3faf1b0957148cb5673aee56715bd66793aa2700
LA-10 <lashuha@uwaterloo.com>
2025-06-15 22:21:38 +0300

    Simplifying logic

--- ch01/ex1_11.cpp
+++ ch01/ex1_11.cpp
@@ -1,23 +1,22 @@
 // Print each number in the range specified by two integers.
 
 #include <iostream>
-using std::cout;
-using std::cin;
-
-void print_range(int low, int high) {
-	if(low > high) {
-		print_range(high, low);
-		return;
-	}
-	for (int i = low; i <= high; ++i) {
-		cout << i << " ";
-	}
-}
 
 int main() {
-	int low = 0, high = 0;
-	cout << "Enter two integers:\n";
-	cin >> low >> high;
-	print_range(low, high);
-	return 0;
+    int num1 = 0;
+    int num2 = 0;
+
+    std::cout << "Enter the first number: "; 
+    std::cin >> num1;
+
+    std::cout << "Enter the second number (must be bigger than " << num1 << "): ";
+    std::cin >> num2;
+
+
+    while (num1 <= num2) {
+        std::cout << num1 << std::endl;
+        num1++;
+    }
+
+    return 0;
 }
